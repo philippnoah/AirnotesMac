ef363c069d1cf6b8c69da02f1895d74710e97949
Ray Zhu <rayzhuau@gmail.com>
2019-06-24 23:04:49 +1000

    init new Airnotes Mac project

---
+++ Airnotes Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Airnotes Mac
+//
+//  Created by Ray Zhu on 24/6/19.
+//  Copyright © 2019 rayzhu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
--- Menu Bar Application/SettingsViewControlelr.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SettingsViewControlelr.swift
-//  Airnotes
-//
-//  Created by Philipp Eibl on 8/22/16.
-//  Copyright © 2016 Philipp Eibl. All rights reserved.
-//
-
-import Foundation
